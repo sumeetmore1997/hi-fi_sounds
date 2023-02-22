9c3875752d96eee7348390151ee6c1872d0cab3c
sumeetmore1997 <101966263+sumeetmore1997@users.noreply.github.com>
2023-02-22 23:35:23 +0530

    products is added to the database

--- backend/routes/Products.route.js
+++ backend/routes/Products.route.js
@@ -5,39 +5,49 @@ const { EarbudsModel, RockerzModel }=require("../models/Products.model")
 
 // --------earbuds---------
 
-productRoute.get("/earbuds",(req,res) => {
-    res.send("All the Products here")
+const productsModel={earbuds:EarbudsModel,rockerz:RockerzModel}
+
+productRoute.get("/:category",async(req,res) => {
+    console.log(req.params)
+    const earbuds=await productsModel[req.params.category].find(req.body)
+    res.send({"msg":`All the ${req.params.category} Products here`,data:earbuds})
 })
 
-productRoute.post("/earbuds/add",async(req,res) => {
+productRoute.post("/:category/add",async(req,res) => {
     const payload=req.body
     try{
-        const new_product=new EarbudsModel(payload)
+        let new_product
+        // for (let i=0; i<payload.length; i++)
+        // {
+            //console.log(productsModel[req.params.category])
+            new_product=new productsModel[req.params.category](payload)
         await new_product.save()
-        res.send("Product is added")
+        // }
+        //res.send("Product is added")
+        res.send({"msg":`Product is added in ${req.params.category}`,data:new_product})
     }catch(err){
         console.log(err)
-        res.send({"msg":"Something went wrong"})
+        res.send({"msg":"Something went wrong",err:err.message})
     }
 })
 
 // ---------rockerz--------
 
-productRoute.get("/rockerz",(req,res) => {
-    res.send("All the Products here")
-})
+// productRoute.get("/rockerz",(req,res) => {
+//     res.send("All the Products here")
+// })
 
-productRoute.post("/rockerz/add",async(req,res) => {
-    const payload=req.body
-    try{
-        const new_product=new RockerzModel(payload)
-        await new_product.save()
-        res.send("Product is added")
-    }catch(err){
-        console.log(err)
-        res.send({"msg":"Something went wrong"})
-    }
-})
+// productRoute.post("/rockerz/add",async(req,res) => {
+//     const payload=req.body
+//     try{
+//         const new_product=new RockerzModel(payload)
+//         await new_product.save()
+//         res.send("Product is added")
+//     }catch(err){
+//         console.log(err)
+//         res.send({"msg":"Something went wrong"})
+//     }
+// })
 
 
 module.exports={
